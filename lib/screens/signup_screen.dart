5619d38d955405e3c6710f7445875f02b4b4ee85
navendunandu <navendunandu@gmail.com>
2024-02-23 11:53:06 +0530

    registration design complete

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 // import 'package:icons_plus/icons_plus.dart';
 import 'package:flutter_firebase/screens/signin_screen.dart';
 import 'package:flutter_firebase/theme/theme.dart';
 import 'package:flutter_firebase/widgets/custom_scaffold.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -14,6 +17,43 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final _formSignupKey = GlobalKey<FormState>();
   bool agreePersonalData = true;
+  XFile? _selectedImage;
+  String? _imageUrl;
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _dobController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _passController = TextEditingController();
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = XFile(pickedFile.path);
+      });
+    }
+  }
+
+  List<Map<String, dynamic>> district = [
+    {'id': 'ernakulam', 'name': 'Ernakulam'},
+    {'id': 'idukki', 'name': 'Idukki'},
+    {'id': 'kottayam', 'name': 'Kottayam'},
+  ];
+
+  String? selectdistrict;
+
+  List<Map<String, dynamic>> place = [
+    {'id': 'muvattupuzha', 'name': 'Muvattupuzha'},
+    {'id': 'aluva', 'name': 'Aluva'},
+    {'id': 'guruvayoor', 'name': 'Guruvayoor'},
+  ];
+
+  String? selectplace;
+
+  String? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
@@ -53,10 +93,57 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         ),
                       ),
                       const SizedBox(
-                        height: 40.0,
+                        height: 20.0,
+                      ),
+                      Center(
+                        child: GestureDetector(
+                          onTap: _pickImage,
+                          child: Stack(
+                            children: [
+                              CircleAvatar(
+                                radius: 50,
+                                backgroundColor: const Color(0xff4c505b),
+                                backgroundImage: _selectedImage != null
+                                    ? FileImage(File(_selectedImage!.path))
+                                    : _imageUrl != null
+                                        ? NetworkImage(_imageUrl!)
+                                        : const AssetImage(
+                                                'assets/images/dummy-profile-pic.png')
+                                            as ImageProvider,
+                                child: _selectedImage == null &&
+                                        _imageUrl == null
+                                    ? const Icon(
+                                        Icons.add,
+                                        size: 40,
+                                        color:
+                                            Color.fromARGB(255, 134, 134, 134),
+                                      )
+                                    : null,
+                              ),
+                              if (_selectedImage != null || _imageUrl != null)
+                                const Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    radius: 18,
+                                    child: Icon(
+                                      Icons.edit,
+                                      size: 18,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
                       ),
                       // full name
                       TextFormField(
+                        controller: _nameController,
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please enter Full name';
@@ -88,6 +175,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       ),
                       // email
                       TextFormField(
+                        controller: _emailController,
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please enter Email';
@@ -117,8 +205,214 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       const SizedBox(
                         height: 25.0,
                       ),
+                      TextFormField(
+                        controller: _dobController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Email';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.datetime,
+                        decoration: InputDecoration(
+                          label: const Text('DOB'),
+                          hintText: 'Enter Date of Birth',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5, right: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Gender: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  activeColor: Colors.blue,
+                                  value: 'Male',
+                                  groupValue: selectedGender,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedGender = value!;
+                                    });
+                                  },
+                                ),
+                                const Text('Male')
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  activeColor: Colors.blue,
+                                  value: 'Female',
+                                  groupValue: selectedGender,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedGender = value!;
+                                    });
+                                  },
+                                ),
+                                const Text('Female')
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  activeColor: Colors.blue,
+                                  value: 'Others',
+                                  groupValue: selectedGender,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedGender = value!;
+                                    });
+                                  },
+                                ),
+                                const Text('Others')
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      DropdownButtonFormField<String>(
+                        value: selectdistrict,
+                        decoration: InputDecoration(
+                          label: const Text('District'),
+                          hintText: 'Select District',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectdistrict = newValue;
+                          });
+                        },
+                        isExpanded: true,
+                        items: district.map<DropdownMenuItem<String>>(
+                          (Map<String, dynamic> dist) {
+                            return DropdownMenuItem<String>(
+                              value: dist['id'],
+                              child: Text(dist['name']),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      DropdownButtonFormField<String>(
+                        value: selectplace,
+                        decoration: InputDecoration(
+                          label: const Text('Place'),
+                          hintText: 'Select Place',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectplace = newValue;
+                          });
+                        },
+                        isExpanded: true,
+                        items: place.map<DropdownMenuItem<String>>(
+                          (Map<String, dynamic> place) {
+                            return DropdownMenuItem<String>(
+                              value: place['id'],
+                              child: Text(place['name']),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: _addressController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Email';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.multiline,
+                        decoration: InputDecoration(
+                          label: const Text('Address'),
+                          hintText: 'Enter Address',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
                       // password
                       TextFormField(
+                        controller: _passController,
                         obscureText: true,
                         obscuringCharacter: '*',
                         validator: (value) {
